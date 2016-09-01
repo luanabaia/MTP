62b0d58867ff2f014dc89e1f64e172ee6144286c
luanabaia <luanabaiasousa@gmai.com>
2016-09-01 12:15:19 -0300

    fim da aula

--- aula20160901/mat3.c
+++ aula20160901/mat3.c
@@ -4,14 +4,14 @@
 
 int main ()
 {
-    double base, numero;
+    double base, numero, logaritmo;
     double log10(double numero);
     printf("\nEntre com um valor qualquer\n\n");
     scanf("%lf", &numero);
     printf("\nO valor da base e 10, digite 10:\n\n");
     scanf("%lf", &base);
-    log10 numero = (log(numero) / log(base)) ;
-    printf("\nO valor do logaritmo  e: %lf.\n", a);
+    logaritmo = log(numero)/log(base);
+    printf("\nO valor do logaritmo e: %lf\n",logaritmo);
     return 0;
 }
 
