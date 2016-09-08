4383cc6ffb97e1f5134e9fb0975bb264671551f5
luanabaia <luanabaiasousa@gmail.com>
2016-09-08 12:17:18 -0300

    fim da aula

--- aula20160908/vet3.c
+++ aula20160908/vet3.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <stdlib.h> //ponteiro
+#define N 100
+int main()
+{
+    int vetor[100], i, n, soma, prod;
+    printf ("Digite o comprimento da sequencia: \n\n");
+    scanf ("\n%d", &n);
+    printf ("\nDigite uma sequencia com %d numeros inteiros: \n", n );
+    for ( i = 0 ; i < n; i ++)
+    {
+       scanf ("\n%d", &vetor [i]) ;
+    }
+    return 0;
+}
